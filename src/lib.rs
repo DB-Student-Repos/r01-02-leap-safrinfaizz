8e1cbc214a80e3d376b16bdf6ce9754f3eb79e4a
safrin <safreenakokkadan@gmail.com>
2024-06-14 20:09:44 +0200

    your-commit-message

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
 pub fn is_leap_year(year: u64) -> bool {
-    unimplemented!("true if {year} is a leap year")
+    if year % 4 == 0 {
+        if year % 100 == 0 {
+            if year % 400 == 0 {
+                true
+            } else {
+                false
+            }
+        } else {
+            true
+        }
+    } else {
+        false
+    }
 }
+
